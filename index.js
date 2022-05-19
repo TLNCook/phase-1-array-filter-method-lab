ef089101b44631ee43f753589c02042df4ae525b
Thomas Cook <tlncook85@gmail.com>
2022-05-19 15:32:49 -0600

    commit tree 526041c6f2b044d904515fe3bce560fd82167e39

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+
+function findMatching(drivers, name) {
+    return drivers.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === name.toLowerCase()
+    );
+  }
+  
+function fuzzyMatch(drivers, testName) {
+    return drivers.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testName.toLowerCase()) === 0
+    );
+  }
+  
+function matchName(drivers, driverName) {
+    return drivers.filter((driver) => driver.name === driverName);
+  }
